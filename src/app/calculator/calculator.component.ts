2cebd0e84763a591785d8c35d43f3dc03afde798
babols <julien.delpeut@laposte.net>
2022-10-24 16:49:11 +0200

    add function

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
+  currentValue = [];
+  secondValue= 0;
 
-  constructor() { }
+  getNumber(id: number) {
 
-  ngOnInit(): void {
+    console.log(id);
   }
+  getOperator(op: string) {
+    console.log(op);
+  }
+  getComma(cma: string) {
+    console.log(cma);
+  }
+  getClear(clr: string) {
+    console.log(clr);
+  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
